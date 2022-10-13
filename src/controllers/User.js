d0e7b82b60139c9b284a6e27af4e6e3501b68764
daniellopes <dl.lopes2011@gmail.com>
2022-10-13 22:22:17 +0100

    Update User.js

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -50,8 +50,6 @@ module.exports = {
     }
 
     if (user) {
-      res.header("Access-Control-Allow-Origin", "https://change-password.hsmedsaude.com");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.status(200).json({
         user: {
           _id: user._id,
